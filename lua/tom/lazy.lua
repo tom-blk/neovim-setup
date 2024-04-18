eacbc5aae870ec844b073764937df2ceebea5707
Tom Bleek <67538825+tom-blk@users.noreply.github.com>
2024-04-18 21:42:35 +0200

    Removed unnecessary comments

--- lua/tom/lazy.lua
+++ lua/tom/lazy.lua
@@ -27,9 +27,7 @@ local plugins = {
     		},
 		opts = {
       		debug = true, -- Enable debugging
-      -- See Configuration section for rest
     		},
-    -- See Commands section for default commands if you want to lazy load on them
   	},
 	{
 		"nvim-treesitter/nvim-treesitter",
